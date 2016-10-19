5f340400075b740e6bc1764720a846d87bbddee4
fruittaco <mtv25@case.edu>
2016-10-19 00:34:54 -0400

    Excluded java7.class files for matlab from .gitignore Commited java classes for matlab Added display parameter to matlab

--- src/matlab/DotGrid.m
+++ src/matlab/DotGrid.m
@@ -1,14 +1,16 @@
 
-function adjacencyMatrix = DotGrid (file, width)
+function adjacencyMatrix = DotGrid (file, width, show)
     image = imread(file);    
     imshow(image)
     [x,y] = ginput(2);
     x = round(x/width)
     y = round(y/width)
-    adjacencyMatrix = FindConnections(file, width);
+    adjacencyMatrix = FindConnections(file, width, display);
     aStarAgent = javaObjectEDT('Pathfinding.AStar');
     path = javaMethod('AStarSearch', aStarAgent, [x(1) y(1)], [x(2) y(2)], adjacencyMatrix)
-    DisplayPath(image, width, path);
+    if show
+        DisplayPath(image, width, path);
+    end
 end
 
 function DisplayPath(image, width, path)
@@ -29,14 +31,16 @@ function DisplayPath(image, width, path)
             xPixel = xPixel + xDir;
 end
     end
-    %figure
+    figure
     imshow(image)
 end
 
 %read the image pointed to by file witha dot spacing of width
-function adjacencyMatrix = FindConnections (file, width)
+function adjacencyMatrix = FindConnections (file, width, show)
     image = imread(file);
-    display = image;
+    if show
+        display = image;
+    end
     yBound = size(display, 1);%rows
     yLength = floor(yBound/width);
     xBound = size(display, 2);%columns
@@ -55,47 +59,57 @@ function adjacencyMatrix = FindConnections (file, width)
               %east
               if (x+1 <= xLength) & (display(yPixel, xPixel+1:xPixel+width-1,:) == 255)
                   javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x+1 y]-1);%account for 1->0 indexing
-                  display(yPixel, xPixel+1:xPixel+width-1,1) = 131;
-                  display(yPixel, xPixel+1:xPixel+width-1,2) = 255;
-                  display(yPixel, xPixel+1:xPixel+width-1,3) = 119;
+                  if show
+                      display(yPixel, xPixel+1:xPixel+width-1,1) = 131;
+                      display(yPixel, xPixel+1:xPixel+width-1,2) = 255;
+                      display(yPixel, xPixel+1:xPixel+width-1,3) = 119;
+                  end
               end
               %southeast
               if (x+1 <= xLength) & (y+1 <= yLength) & CheckDiagonal([y x], [y+1 x+1], image, width)
                   javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x+1 y+1]-1);%account for 1->0 indexing
-                  r = yPixel+1;
-                  c = xPixel+1;
-                  for i=1:width-1
-                      display(r,c,1)= 131;
-                      display(r,c,2)= 255;
-                      display(r,c,3)= 119;
-                      r = r+1;
-                      c = c+1;
+                  if show
+                      r = yPixel+1;
+                      c = xPixel+1;
+                      for i=1:width-1
+                          display(r,c,1)= 131;
+                          display(r,c,2)= 255;
+                          display(r,c,3)= 119;
+                          r = r+1;
+                          c = c+1;
+                      end
                   end
               end
               %south
               if (y+1 <= yLength) & (display(yPixel+1:yPixel+width-1, xPixel,:) == 255)
                   javaMethod('addAdjacency', adjacencyMatrix, [x y]-1, [x y+1]-1);%account for 1->0 indexing
-                  display(yPixel+1:yPixel+width-1, xPixel,1) = 131;
-                  display(yPixel+1:yPixel+width-1, xPixel,2) = 255;
-                  display(yPixel+1:yPixel+width-1, xPixel,3) = 119;
+                  if show
+                      display(yPixel+1:yPixel+width-1, xPixel,1) = 131;
+                      display(yPixel+1:yPixel+width-1, xPixel,2) = 255;
+                      display(yPixel+1:yPixel+width-1, xPixel,3) = 119;
+                  end
               end
               %southwest
               if (x-1 >= 1) & (y+1 <= yLength) & CheckDiagonal([y x], [y+1 x-1], image, width)
                   javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x-1 y+1]-1);%account for 1->0 indexing
-                  r = yPixel+1;
-                  c = xPixel-1;
-                  for i=1:width-1
-                      display(r,c,1)= 131;
-                      display(r,c,2)= 255;
-                      display(r,c,3)= 119;
-                      r = r+1;
-                      c = c-1;
+                  if show
+                      r = yPixel+1;
+                      c = xPixel-1;
+                      for i=1:width-1
+                          display(r,c,1)= 131;
+                          display(r,c,2)= 255;
+                          display(r,c,3)= 119;
+                          r = r+1;
+                          c = c-1;
+                      end
                   end
               end
           end
        end
     end
-    imshow(display)
+    if show
+        imshow(display)
+    end
 end
 
 function valid = CheckDiagonal(point1, point2, image, width)
