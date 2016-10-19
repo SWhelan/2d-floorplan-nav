75b21fd2175725d1d87a605a1a9093f466f65bf8
fruittaco <mtv25@case.edu>
2016-10-19 00:09:11 -0400

    Added diagonal connections

--- src/matlab/DotGrid.m
+++ src/matlab/DotGrid.m
@@ -15,13 +15,19 @@ function DisplayPath(image, width, path)
     for i=1:size(path,1)-1
         p1 = path(i,:);
         p2 = path(i+1,:);
-        x1 = min([p1(1) p2(1)]);
-        x2 = max([p1(1) p2(1)]);
-        y1 = min([p1(2) p2(2)]);
-        y2 = max([p1(2) p2(2)]);
-        image(y1*width:y2*width, x1*width:x2*width, 1) = 77;
-        image(y1*width:y2*width, x1*width:x2*width, 2) = 56;
-        image(y1*width:y2*width, x1*width:x2*width, 3) = 153;
+        xDir = p2(1) - p1(1);
+        xDir = xDir./abs(xDir);
+        yDir = p2(2) - p1(2);
+        yDir = yDir./abs(yDir);
+        xPixel = p1(1)*width;
+        yPixel = p1(2)*width;
+        for i=1:width
+            image(yPixel, xPixel, 1) = 77;
+            image(yPixel, xPixel, 2) = 56;
+            image(yPixel, xPixel, 3) = 153;
+            yPixel = yPixel + yDir;
+            xPixel = xPixel + xDir;
+end
     end
     %figure
     imshow(image)
@@ -29,103 +35,80 @@ end
 
 %read the image pointed to by file witha dot spacing of width
 function adjacencyMatrix = FindConnections (file, width)
-image = imread(file);
-display = image;
-yBound = size(display, 1);%rows
-yLength = floor(yBound/width);
-xBound = size(display, 2);%columns
-xLength = floor(xBound/width);
-adjacencyMatrix = javaObjectEDT('Pathfinding.AdjacencyMatrix', xBound, yBound);
-for y=1:yLength
-   for x=1:xLength
-      xPixel = x*width;
-      yPixel = y*width;
-      if display(yPixel,xPixel,:) == 255
-          %color the dot black
-          display(yPixel,xPixel,:) = 0;
-          %if the next horizontal point is in bounds and the image is clear between
-          %them, add the adjacency to both points
-          if (x+1 <= xLength) & (display(yPixel, xPixel+1:xPixel+width-1,:) == 255)
-              javaMethod('addAdjacency', adjacencyMatrix,[x-1 y-1], [x+1-1 y-1]);%account for 1->0 indexing
-              display(yPixel, xPixel+1:xPixel+width-1,1) = 131;
-              display(yPixel, xPixel+1:xPixel+width-1,2) = 255;
-              display(yPixel, xPixel+1:xPixel+width-1,3) = 119;
+    image = imread(file);
+    display = image;
+    yBound = size(display, 1);%rows
+    yLength = floor(yBound/width);
+    xBound = size(display, 2);%columns
+    xLength = floor(xBound/width);
+    adjacencyMatrix = javaObjectEDT('Pathfinding.AdjacencyMatrix', xBound, yBound);
+    for y=1:yLength
+       for x=1:xLength
+          xPixel = x*width;
+          yPixel = y*width;
+          if display(yPixel,xPixel,:) == 255
+              %color the dot black
+              display(yPixel,xPixel,:) = 0;
+              %Check east, southeast, south, southwest points
+                %first check point is in bounds then check that the pixels
+                %between the points are clear
+              %east
+              if (x+1 <= xLength) & (display(yPixel, xPixel+1:xPixel+width-1,:) == 255)
+                  javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x+1 y]-1);%account for 1->0 indexing
+                  display(yPixel, xPixel+1:xPixel+width-1,1) = 131;
+                  display(yPixel, xPixel+1:xPixel+width-1,2) = 255;
+                  display(yPixel, xPixel+1:xPixel+width-1,3) = 119;
+              end
+              %southeast
+              if (x+1 <= xLength) & (y+1 <= yLength) & CheckDiagonal([y x], [y+1 x+1], image, width)
+                  javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x+1 y+1]-1);%account for 1->0 indexing
+                  r = yPixel+1;
+                  c = xPixel+1;
+                  for i=1:width-1
+                      display(r,c,1)= 131;
+                      display(r,c,2)= 255;
+                      display(r,c,3)= 119;
+                      r = r+1;
+                      c = c+1;
+                  end
+              end
+              %south
+              if (y+1 <= yLength) & (display(yPixel+1:yPixel+width-1, xPixel,:) == 255)
+                  javaMethod('addAdjacency', adjacencyMatrix, [x y]-1, [x y+1]-1);%account for 1->0 indexing
+                  display(yPixel+1:yPixel+width-1, xPixel,1) = 131;
+                  display(yPixel+1:yPixel+width-1, xPixel,2) = 255;
+                  display(yPixel+1:yPixel+width-1, xPixel,3) = 119;
+              end
+              %southwest
+              if (x-1 >= 1) & (y+1 <= yLength) & CheckDiagonal([y x], [y+1 x-1], image, width)
+                  javaMethod('addAdjacency', adjacencyMatrix,[x y]-1, [x-1 y+1]-1);%account for 1->0 indexing
+                  r = yPixel+1;
+                  c = xPixel-1;
+                  for i=1:width-1
+                      display(r,c,1)= 131;
+                      display(r,c,2)= 255;
+                      display(r,c,3)= 119;
+                      r = r+1;
+                      c = c-1;
+                  end
+              end
           end
-          %if the next vertical point is in bounds and the image is clear between
-          %them, add the adjacency to both points
-          if (y+1 <= yLength) & (display(yPixel+1:yPixel+width-1, xPixel,:) == 255)
-              javaMethod('addAdjacency', adjacencyMatrix, [x-1 y-1], [x-1 y+1-1]);%account for 1->0 indexing
-              display(yPixel+1:yPixel+width-1, xPixel,1) = 131;
-              display(yPixel+1:yPixel+width-1, xPixel,2) = 255;
-              display(yPixel+1:yPixel+width-1, xPixel,3) = 119;
-          end
-      end
-   end
-end
-imshow(display)
-end
-
-function r = Heuristic(start, goal)
-r = abs(start(1)-goal(1)) + abs(start(2)-goal(2));
-end
-
-%Takes adjacency matrix and two ordered pairs start, end
-function AStarSearch(AdjacencyMatrix, start, goal)
-%[cost, heuristic value, function value, parent x, parent y]
-SearchMatrix = cell(size(AdjacencyMatrix, 1), size(AdjacencyMatrix,2));
-%initialize starting location
-SearchMatrix{start(1), start(2), 1} = 0;
-SearchMatrix{start(1), start(2), 2} = Heuristic(start, goal);
-SearchMatrix{start(1), start(2), 3} = Heuristic(start, goal);
-%TODO: learn how to add a comparator to the priority queue
-OpenList = java.util.PriorityQueue();
-ClosedList = java.util.LinkedList();
-OpenList.add(SearchMatrix{start(1), start(2)});
-Path = [];
-while OpenList.size()>0
-    if ExploreLocation(SearchMatrix, AdjacencyMatrix, goal, OpenList, ClosedList)
-       %create the path
-       break;
+       end
     end
-end
-if size(Path,1)>0
-   %do good things
-else
-   %do bad things 
-end
+    imshow(display)
 end
 
-function goalReached = ExploreLocation(SearchMatrix, AdjacencyMatrix, goal, OpenList, ClosedList)
-goalReached = false;
-location = OpenList.poll();
-for i=1:AdjacencyMatrix{location(0), location(1),:}
-   if CheckLocation(location, SearchMatrix, AdjacencyMatrix, goal, OpenList, ClosedList)
-       goalReached=true;
-   end
-end
-end
-
-function goalReached = CheckLocation(location, SearchMatrix, AdjacencyMatrix, goal, OpenList, ClosedList)
-goalReached = location(1) == goal(1) && location(2) == goal(2);
-valid = true;
-%check closed list
-for i=1:ClosedList.size()
-   check = ClosedList.get(i);
-   if location(1)==check(1) && location(2)==check(2)
-      valid = false; 
-   end
-end
-%check open list
-if valid
-   check = OpenList.get(i);
-   for i=1:OpenList.size()
-      if location(1)==check(1) && location(2)==check(2)
-          valid = false;
-      end
-   end
-end
-%add to open list
-if valid
-       
-end
+function valid = CheckDiagonal(point1, point2, image, width)
+    yDir = point2(1) - point1(1);
+    yDir = yDir./abs(yDir);
+    xDir = point2(2) - point1(2);
+    xDir = xDir./abs(xDir);
+    valid = 1;
+    yPixel = point1(1)*width+yDir;
+    xPixel = point1(2)*width+xDir;
+    for i=1:width-1
+        valid = valid & image(yPixel,xPixel,:)==255;
+        yPixel = yPixel + yDir;
+        xPixel = xPixel + xDir;
+    end
 end
